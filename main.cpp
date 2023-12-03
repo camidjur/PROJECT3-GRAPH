fa48dbcaa5325d7270a272f5238e7a2819b918d9
camila <70236659+camidjur@users.noreply.github.com>
2023-12-03 18:34:33 -0500

    Update main.cpp
    adding comments and function purposes

--- main.cpp
+++ main.cpp
@@ -4,7 +4,14 @@
 using namespace std;
 
 int getTransportationMode() {
+    /* The purpose of this function is to provide the user with different modes of 
+    * transportation that can be used to travel from one vertex to another. This 
+    * will alter the time needed to travel this distance and the gas emissions it 
+    * produces in the process. 
+    */
     int mode = 0;
+
+    // menu list 
     cout << "Select Transportation Mode:" << endl;
     cout << "1. Walking" << endl;
     cout << "2. Biking" << endl;
@@ -13,8 +20,10 @@ int getTransportationMode() {
     cout << "Enter your choice: ";
     cin >> mode;
 
+    // validating input
     if (mode >= 1 && mode <= 4) {
-        return mode; // Valid input, break the loop
+        return mode;
+    // if the input is not valid, recursively calls function until valid input
     } else {
         cout << "Invalid choice. Please try again." << endl;
         return getTransportationMode();
@@ -22,15 +31,23 @@ int getTransportationMode() {
 }
 
 int getSearchAlgorithm() {
+    /* The purpose of this function is to provide the user with a choice as to
+    * which shortest path algorithm they want the program to use to find the 
+    * path from one vertex to another. 
+    */
     int algorithm;
+
+    // menu list
     cout << "Choose Search Algorithm:" << endl;
     cout << "1. Bellman Ford's Search" << endl;
     cout << "2. Dijkstra’s Search" << endl;
     cout << "Enter your choice: ";
     cin >> algorithm;
 
+    // input validation
     if (algorithm == 1 || algorithm == 2) {
-        return algorithm; // Valid input, break the loop
+        return algorithm; 
+    // if input is not valid, recursively call function until valid input
     } else {
         cout << "Invalid choice. Please try again." << endl;
         return getSearchAlgorithm();
@@ -38,6 +55,12 @@ int getSearchAlgorithm() {
 }
 
 void getStartAndEndPoints(Coordinate &startPoint, Coordinate &endPoint) {
+    /* The purpose of this function is to allow the user to enter a latitude and 
+    * a longitude for the source vertex as well as a latitude and longitude for the
+    * target vertex. This is later checked with a data set that holds all the existing data 
+    * points for Gainesville to validate the input. 
+    */
+    // source vertex
     cout << "Enter the starting latitude coordinate: ";
     cin.ignore(); // To clear the return character from the buffer
     string latitude;
@@ -50,6 +73,7 @@ void getStartAndEndPoints(Coordinate &startPoint, Coordinate &endPoint) {
     double lon1 = std::stod(longitude);
     startPoint = Coordinate {lat1, lon1};
 
+    // target vertex
     cout << "Enter the ending latitude coordinate: ";
     getline(cin, latitude);
     double lat2 = std::stod(latitude);
@@ -73,11 +97,13 @@ int main() {
     cout << endl;
 
     mode = getTransportationMode();
+    // sets up map to utilize as graph representation
     std::map<Coordinate, std::vector<std::pair<double, Coordinate>>> data = getGraph(mode);
     algorithm = getSearchAlgorithm();
+    // calls for input
     getStartAndEndPoints(startPoint, endPoint);
 
-    // wait for valid input
+    // wait for valid input if initial input was not in data set 
     while (data.find(startPoint) == data.end() || data.find(startPoint) == data.end()) {
         cout << "Source or target coordinates not found in the graph." << endl;
         getStartAndEndPoints(startPoint, endPoint);
